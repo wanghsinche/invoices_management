04c8c8b1e3364d9af1cdb3fea75217014877a25e
peng.zhang <peng.zhang@datayes.com>
2016-03-15 15:37:09 +0800

    first commit

---
+++ src/ui/jsx/securityChooser.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Market = require('./Market');
+
+var SecurityChooser = React.createClass({
+	render : function() {
+		return <input value=""/>;
+	}
+});
+module.exports = SecurityChooser;
